a0fc8f94e559aaf20b13ac89aba9877e1a4c11ea
tristantao <tao.tristan@gmail.com>
2014-04-21 16:50:21 -0700

    reorg

--- analysis/lm.R
+++ analysis/lm.R
@@ -4,12 +4,29 @@ source('analysis/util.R')
 allNBA <- read.csv("analysis/joined.csv", header = TRUE, stringsAsFactors = FALSE)
 allNBA$Date = as.Date(allNBA$Date, "%d-%m-%Y") #format: 17-04-2013
 
-template = data.frame(Team1 = character(),
-                      Team2 = character(),
-                      Date = as.Date(character()),
-                      Team1_Score = numeric(),
-                      Team2_Score = numeric())
-#Last month's histroy for both side
+prepLM = function(dataframe, outputData) { #LM PREP?
+  if (nrow(dataframe) > 0) { #this should reflect template
+    appendFrame = data.frame(Team1 = dataframe$Team1[1],
+                             Team2 = dataframe$Team2[1],
+                             Date = dataframe$Date[1],
+                             Team1_Score = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]),
+                             Team2_Score = sum(dataframe$Points.Scored[dataframe$Team2 == dataframe$Team])
+    )
+    outputData = rbind(outputData, appendFrame)
+  }
+  return (outputData)
+}
+
+simpleAggr = getPerGameData(allNBA, simple_aggr_template, aggrBasic)
+
+for (simple_index in 1:nrow(simpleAggr)){ 
+  
+}
+
+simpleAggr$Team1 = as.character(simpleAggr$Team1)
+simpleAggr$Team2 = as.character(simpleAggr$Team2)
+
+
 "
 Team1_win_withint_last_6
 Team1_away_win_percentage
