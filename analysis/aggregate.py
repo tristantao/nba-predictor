a305a80b0c3a633a863d48893acd606ce6782130
tristantao <tao.tristan@gmail.com>
2014-04-20 18:31:46 -0700

    aggration complete

--- analysis/aggregate.py
+++ analysis/aggregate.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import csv
 
 os.environ['ROOT'] = '/Users/t-rex-Box/Desktop/work/nba-predictor/'
 
@@ -31,19 +32,50 @@ def get_catalog_dict(catalog_dir):
 	return catalogs
 
 
-def join_boxscores(catalog_dir, output_dir):
+def join_boxscores(catalog_dir, output_dir_path):
 	'''
 	Given a directory, this function will recursively find all boxscores.
 	It will then join the data into a single csv (while adding 3 more columans, team1, team2 and date)
 	to output to output_dir,
 	'''
 	box_catalog = get_catalog_dict(catalog_dir)
+	static_header = None
+	entries = 0
+	with open(output_dir_path, 'w') as fp:
+		out_csv = csv.writer(fp, delimiter=',')
+		#Now go through the files that are in boxscore file and go through it.
+		for catalog_path, catalog_name in box_catalog.items():
+			team1, team2, date =parse_boxscore_names(catalog_name)
+			with open(catalog_path, 'rb') as curent_f:
+				reader = csv.reader(curent_f)
+				header = reader.next()
+				if static_header == None:  #just grab the first one
+					static_header = ['Team1','Team2','Date'] + header
+					out_csv.writerow(static_header)
+				for row in reader:
+					new_row = [team1, team2, date] + row
+					out_csv.writerow(new_row)
+					entries += 1
+	print "Joined %s Entries at %s" % (entries, output_dir_path)
+
+
+join_boxscores(os.path.join(os.environ['ROOT'], 'data/box_scores/'),
+	os.path.join(os.environ['ROOT'], 'analysis/', 'joined.csv'))
 
 
 
 
 
-print join_boxscores(os.path.join(os.environ['ROOT'], 'data/box_scores/'), 'pass.csv')
+
+
+
+
+
+
+
+
+
+
 
 
 
