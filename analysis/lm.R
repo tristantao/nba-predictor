eca660f7d12a251a973b1562c7a3e9862480f9a6
tristantao <tao.tristan@gmail.com>
2014-04-26 18:06:09 -0700

    last_date_game -> last_days_game + fix

--- analysis/lm.R
+++ analysis/lm.R
@@ -18,58 +18,38 @@ feature_vectors$Team2_away_win_percentage_10 = NA
 feature_vectors$Team1_avg_pnt_top_3_players_6 = NA
 feature_vectors$Team2_avg_pnt_top_3_players_6 = NA
 
-lm_generic_template = data.frame(Team1 = character(), #This will be used by both team1 and team2, so generic data points
-                         Team2 = character(),
-                         Team_Score = numeric(),
-                         Date = as.Date(character()),
-                         Team_win_last_6 = numeric(),
-                         Team_away_win_percentage_10 = numeric(),
-                         Team_avg_pnt_top_3_players_6 = numeric())
-
-
-last_x_games = function(data, x, team, game_date) {
-  #Given x and data, it'll return data from up to x games ago, regardless of data.
+last_days_games = function(data, days, team, game_date) {
+  #Given x and data, it'll return data from up to x days ago, regardless of data.
   #Data needs a $Team1, $Team2, and $Date column.
   #Usage: last_x_games(simpleAggr, 10, 'Utah', as.Date('2013-04-17'))
-  sub_table = data[data$Team1 == team |
-                     data$Team2 == team &
-                     (data$Date > game_date + 100) #look back 100 days
+  sub_table = data[(data$Team1 == team |
+                     data$Team2 == team) &
+                     (data$Date - days) < game_date #look back 100 days
                    ,]
   sub_table = sub_table[with(sub_table, order(Date, decreasing=TRUE)), ]
-  return (sub_table[1:x,])
-}
-
-lm_feature_aggr = function(dataframe, outputData) {
-  #Given a dataframe for a single game, returns the aggregated data that includes the features.
-  if (nrow(dataframe) > 0) { #this should reflect template
-    appendFrame = data.frame(Team1 = dataframe$Team1[1],
-                             Team2 = dataframe$Team2[1],
-                             Date = dataframe$Date[1],
-                             Team1_Score = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]),
-                             Team2_Score = sum(dataframe$Points.Scored[dataframe$Team2 == dataframe$Team]),
-                             Team_win_last_6 = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]),
-                             Team_away_win_percentage_10 = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]),
-                             Team_avg_pnt_top_3_players_6 = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]))
-    outputData = rbind(outputData, appendFrame)
-  }
-  return (outputData)
+  return (sub_table)
 }
 
 for (simple_index in 1:nrow(simpleAggr)){
-  team1_sub_hist = last_x_games(allNBA,
+  #first, get data from last x games. @TODO make sure enough data.
+  team1_sub_hist = last_days_games(allNBA,
                                 100,
                                 simpleAggr[simple_index,]$Team1,
                                 simpleAggr[simple_index,]$Date)
-  team2_sub_hist = last_x_games(allNBA,
-                                100,
+  team2_sub_hist = last_days_games(allNBA,
+                                200,
                                 simpleAggr[simple_index,]$Team2,
                                 simpleAggr[simple_index,]$Date)
-  #CANT GET DATA FROM BOTH TEAM -____-
-  team1_custom_consolidated = getPerGameData(team1_sub_hist, lm_generic_template, lm_feature_aggr, lookback=1) #6? hmm
-  feature_vectors = 
+  
+  #CANT GET DATA FROM BOTH TEAM -____- Gotta manually attach it.
+  team1_custom_consolidated = NA
+    #getPerGameData(team1_sub_hist, lm_generic_template, lm_feature_aggr, lookback=1) #6? hmm
+  team2_custom_consolidated = NA
+    #getPerGameData(team2_sub_hist, lm_generic_template, lm_feature_aggr, lookback=1) #6? hmm
+  #feature_vectors = 
   #simpleAggr = getPerGameData(allNBA, template, aggrBasic)
-  print (team1_hist)
-  print (team2_hist)
+  print (head(team1_sub_hist))
+  print (head(team2_sub_hist))
   break
 }
 
@@ -92,3 +72,28 @@ away wins scaled by total number of away games
 
 "
 
+
+lm_generic_template = data.frame(Team1 = character(), #This will be used by both team1 and team2, so generic data points
+                                 Team2 = character(),
+                                 Team_Score = numeric(),
+                                 Date = as.Date(character()),
+                                 Team_win_last_6 = numeric(),
+                                 Team_away_win_percentage_10 = numeric(),
+                                 Team_avg_pnt_top_3_players_6 = numeric())
+
+
+lm_feature_aggr = function(dataframe, outputData) {
+  #Given a dataframe for a single game, returns the aggregated data that includes the features.
+  if (nrow(dataframe) > 0) { #this should reflect template
+    appendFrame = data.frame(Team1 = dataframe$Team1[1],
+                             Team2 = dataframe$Team2[1],
+                             Date = dataframe$Date[1],
+                             Team1_Score = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]),
+                             Team2_Score = sum(dataframe$Points.Scored[dataframe$Team2 == dataframe$Team]),
+                             Team_win_last_6 = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]),
+                             Team_away_win_percentage_10 = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]),
+                             Team_avg_pnt_top_3_players_6 = sum(dataframe$Points.Scored[dataframe$Team1 == dataframe$Team]))
+    outputData = rbind(outputData, appendFrame)
+  }
+  return (outputData)
+}
