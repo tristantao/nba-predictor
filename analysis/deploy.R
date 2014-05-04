1fd6422e813c42df2f2b611a27b561ba240c5808
tristantao <tao.tristan@gmail.com>
2014-05-04 16:50:26 -0700

    deployment of naivebayes working

--- analysis/deploy.R
+++ analysis/deploy.R
@@ -36,12 +36,10 @@ yhat.predict(model_name="nbaGLM", test[1,])
 
 predict.glm(train.glm, newdata = test, type = "response")
 
-
-
 ############
 #####SVM####
 ############
-#source('svm.R')
+#source('analysis/svm.R')
 model.require <- function() {
   library(e1071)
 }
@@ -63,3 +61,29 @@ yhat.deploy ("nbaSVM")
 yhat.svm.result = yhat.predict(model_name="nbaSVM", test)
 mean (yhat.svm.result == test$Result, na.rm=TRUE)
 
+
+############
+#####NaiveBayes####
+############
+#source('analysis/naive_bayes.R')
+model.require <- function() {
+  library(e1071)
+}
+model.transform  <- function(df) {
+  df$Team1_win_last_6 = as.numeric(df$Team1_win_last_6)
+  df$Team2_win_last_6 = as.numeric(df$Team2_win_last_6)
+  df$Team1_away_win_percentage_10 = as.numeric(df$Team1_away_win_percentage_10)
+  df$Team2_away_win_percentage_10 = as.numeric(df$Team2_away_win_percentage_10)
+  df$Team1_avg_pnt_top_3_players_6 = as.numeric(df$Team1_avg_pnt_top_3_players_6)
+  df$Team2_avg_pnt_top_3_players_6 = as.numeric(df$Team2_avg_pnt_top_3_players_6)
+  df
+}
+model.predict <- function(df) {
+  p.hats = predict(train.nb.1, newdata=df)
+  return (data.frame("result"=p.hats))
+}
+yhat.deploy ("nbaNaiveBayes")
+
+yhat.nb.result = yhat.predict(model_name="nbaNaiveBayes", nb_test)
+yhat.nb.result.cleaned <- subset(yhat.nb.result, select = -c(1,length(yhat.nb.result)) )
+mean (unlist(yhat.nb.result.cleaned) == nb_test$Result, na.rm=TRUE)
--- analysis/naive_bayes.R
+++ analysis/naive_bayes.R
@@ -21,15 +21,13 @@ nb_test$Team2_away_win_percentage_10 = round(nb_test$Team2_away_win_percentage_1
 nb_test$Team1_avg_pnt_top_3_players_6 = round(nb_test$Team1_avg_pnt_top_3_players_6, 2)
 nb_test$Team2_avg_pnt_top_3_players_6 = round(nb_test$Team2_avg_pnt_top_3_players_6, 2)
 
-
 train.nb.1 <- naiveBayes(Result ~ Team1_win_last_6 + Team2_win_last_6 + Team1_away_win_percentage_10
                    + Team2_away_win_percentage_10 + Team1_avg_pnt_top_3_players_6
-                   + Team2_avg_pnt_top_3_players_6, data = train)
+                   + Team2_avg_pnt_top_3_players_6, data = nb_test)
 
 summary(train.nb.1)
 
-p.hats.nb = predict(train.nb.1, test, na.rm=TRUE)
-
-mean (p.hats.nb == test$Result, na.rm=TRUE)
+p.hats.nb = predict(train.nb.1, nb_test, na.rm=TRUE)
+mean (p.hats.nb == nb_test$Result, na.rm=TRUE)
 
 
